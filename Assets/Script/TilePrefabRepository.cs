dabbfbac0144f0e4625ed120422a69482b16d1a2
Thiago Berardinelli <thberardinelli@gmail.com>
2023-07-26 00:40:30 -0300

    Changes in TilePrefabRepository SO
    + Added new prefab for TileView
    + TileView colors are now defined by the array of colors in TilePrefabRepository
    + Added new subclass SpecialTileView
    + Added a new list of type SpecialTileView in TilePrefabRepository class
    - Removed of old tile view prefabs from the project"

--- Assets/Script/TilePrefabRepository.cs
+++ Assets/Script/TilePrefabRepository.cs
@@ -3,5 +3,9 @@
 [CreateAssetMenu(fileName = "TilePrefabRepository", menuName = "Gameplay/TilePrefabRepository")]
 public class TilePrefabRepository : ScriptableObject
 {
-    public TileView[] tileTypePrefabList;
+    public TileView tileViewPrefab;
+    public Color[] colors;
+    
+    //public TileView[] tileTypePrefabList;
+    public SpecialTileView[] specialTilePrefabList;
 }
